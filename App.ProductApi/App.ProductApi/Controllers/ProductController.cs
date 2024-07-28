f983328e7a83b0a26cf1084f63d2f9b6b47fabe8
erayozden167 <erayozden167@gmail.com>
2024-07-28 14:57:25 +0300

    Product.api

---
+++ App.ProductApi/App.ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        static private readonly List<Product> _products = new List<Product>();
+        [HttpGet]
+        public List<Product> ProductList()
+        {
+            return _products;
+        }
+        [HttpGet("{id}")]
+        public Product GetProduct(int id)
+        {
+            Product? item = _products.Find(x => x.Id == id);
+                return item;
+        }
+        [HttpPost]
+        public void CreateProduct(Product item)
+        {
+            _products.Add(item);
+        }
+        [HttpPut("{id}")]
+        public void UpdateProduct(int id,Product item)
+        {
+            Product? x = _products.Find(x => x.Id == id);
+            if (x != null)
+            {
+                x.Name = item.Name;
+                x.Price = item.Price;
+                x.Category = item.Category;
+            }
+        }
+        [HttpDelete("{id}")]
+        public void DelProduct(int id)
+        {
+            Product? item = _products.Find(x => x.Id == id);
+            if (item == null)
+            {
+                return;
+            }
+            _products.Remove(item);
+        }
+    }
+}
---
+++ App.ProductApi/App.ProductApi/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace App.ProductApi
+{
+    public class Product
+    {
+        static private int sayac = 0;
+        private int _id;
+        public Product()
+        {
+            sayac++;
+            _id = sayac;
+        }
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Category { get; set; }
+    }
+}
